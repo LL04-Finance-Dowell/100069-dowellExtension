4279b5a198da05568860d66bce92562ecd857201
unknown <eske17.ea@gmail.com>
2023-01-11 14:29:16 +0300

    change extension layout

---
+++ uxlivinglab/src/component/newSidebar/index.jsx
@@ -0,0 +1,85 @@
+import "../sidebar/style.css";
+
+export default function newSidebar() {
+  return (
+    <div
+      data-elementor-type="wp-page"
+      data-elementor-id="8155"
+      class="elementor elementor-8155"
+    >
+      <div class="elementor-inner">
+        <div class="elementor-section-wrap">
+          <section
+            class="elementor-section elementor-top-section elementor-element elementor-element-fa0cef elementor-reverse-tablet elementor-reverse-mobile MAINMENU elementor-section-content-top elementor-section-boxed elementor-section-height-default elementor-section-height-default"
+            data-id="fa0cef"
+            data-element_type="section"
+            id="EXTENSION"
+          >
+            <div class="elementor-container elementor-column-gap-narrow">
+              <div class="elementor-row">
+                <div
+                  class="elementor-column elementor-col-66 elementor-top-column elementor-element elementor-element-4c1413c5"
+                  data-id="4c1413c5"
+                  data-element_type="column"
+                  id="MAINMENULEFT"
+                  data-settings='{"background_background":"classic","motion_fx_motion_fx_scrolling":"yes","motion_fx_devices":["desktop","tablet","mobile"]}'
+                >
+                  <div class="elementor-column-wrap elementor-element-populated">
+                    <div class="elementor-widget-wrap">
+                      <section
+                        class="elementor-section elementor-inner-section elementor-element elementor-element-6ac0308 hidden elementor-section-content-top elementor-section-boxed elementor-section-height-default elementor-section-height-default"
+                        data-id="6ac0308"
+                        data-element_type="section"
+                        id="profileset"
+                      >
+                        <div class="elementor-container elementor-column-gap-no">
+                          <div class="elementor-row">
+                            <div
+                              class="elementor-column elementor-col-100 elementor-inner-column elementor-element elementor-element-4c21edee"
+                              data-id="4c21edee"
+                              data-element_type="column"
+                              id="M1"
+                              data-settings='{"background_background":"classic"}'
+                            >
+                              <div class="elementor-column-wrap elementor-element-populated">
+                                <div class="elementor-widget-wrap">
+                                  <div
+                                    class="elementor-element elementor-element-467aba39 elementor-widget elementor-widget-text-editor"
+                                    data-id="467aba39"
+                                    data-element_type="widget"
+                                    id="membernumber"
+                                    data-widget_type="text-editor.default"
+                                  >
+                                    <div class="elementor-widget-container">
+                                      <div class="elementor-text-editor elementor-clearfix">
+                                        <p>
+                                          <b>
+                                            <i>My Profile</i>
+                                          </b>
+                                        </p>{" "}
+                                      </div>
+                                    </div>
+                                  </div>
+                                  <div
+                                    class="elementor-element elementor-element-3a4f919 elementor-widget elementor-widget-accordion"
+                                    data-id="3a4f919"
+                                    data-element_type="widget"
+                                    data-widget_type="accordion.default"
+                                  ></div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </section>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
+}
--- uxlivinglab/src/component/sidebar/sidebar.jsx
+++ uxlivinglab/src/component/sidebar/sidebar.jsx
@@ -3,34 +3,37 @@ import { RxCross2 } from "react-icons/rx";
 
 export default function Sidebar() {
   return (
-    <div id="grid-container">
-      <div className="power">
-        <i aria-hidden="true" className="fas fa-power-off"></i>
+    <>
+      <div id="grid-container">
+        <div className="power">
+          <i aria-hidden="true" className="fas fa-power-off"></i>
+        </div>
+        <div className="power">
+          <i aria-hidden="true" className="fas fa-sign-in-alt"></i>
+        </div>
+        <div className="power">
+          <i aria-hidden="true" className="fas fa-credit-card"></i>
+        </div>
+        <div className="power">
+          <i aria-hidden="true" className="fas fa-user"></i>
+        </div>
+        <div className="power">
+          <i aria-hidden="true" className="fas fa-headset"></i>
+        </div>
+        <div className="power">
+          <i aria-hidden="true" className="fas fa-bell"></i>
+        </div>
+        <div className="power">
+          <i aria-hidden="true" className="far fa-gem"></i>
+        </div>
+        <div className="power">
+          <i aria-hidden="true" className="fas fa-hand-holding-heart"></i>
+        </div>
       </div>
-      <div className="power">
-        <i aria-hidden="true" className="fas fa-sign-in-alt"></i>
+      <div className="closeContainer">
+        {/* <h6 className="x">X</h6> */}
+        <RxCross2 size={24} color="white" />
       </div>
-      <div className="power">
-        <i aria-hidden="true" className="fas fa-credit-card"></i>
-      </div>
-      <div className="power">
-        <i aria-hidden="true" className="fas fa-user"></i>
-      </div>
-      <div className="power">
-        <i aria-hidden="true" className="fas fa-headset"></i>
-      </div>
-      <div className="power">
-        <i aria-hidden="true" className="fas fa-bell"></i>
-      </div>
-      <div className="power">
-        <i aria-hidden="true" className="far fa-gem"></i>
-      </div>
-      <div className="power">
-        <i aria-hidden="true" className="fas fa-hand-holding-heart"></i>
-      </div>
-      <div>
-        <RxCross2 size={24} color="red" className="close" />
-      </div>
-    </div>
+    </>
   );
 }
