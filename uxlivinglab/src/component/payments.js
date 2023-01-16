111857cd8acae1adcfeb8d66ed54230e09f5b69d
unknown <eske17.ea@gmail.com>
2023-01-16 15:27:09 +0300

    add products

---
+++ uxlivinglab/src/component/payments.js
@@ -0,0 +1,67 @@
+import "./payments.css";
+import MyButton from "./popup/popup";
+function Payments() {
+  return (
+    <div>
+      {/* <MyButton text="payments" /> */}
+      <section
+        className="elementor-section elementor-inner-section elementor-element elementor-element-5d7fce56 hidden elementor-section-boxed elementor-section-height-default"
+        data-id="5d7fce56"
+        data-element_type="section"
+        id="paymentset"
+      >
+        <div className="elementor-container elementor-column-gap-default">
+          <div className="elementor-row">
+            <div
+              className="elementor-column elementor-col-100 elementor-inner-column elementor-element elementor-element-4748b1a2"
+              data-id="4748b1a2"
+              data-element_type="column"
+              data-settings='{"background_background":"classic"}'
+            >
+              <div className="elementor-column-wrap elementor-element-populated">
+                <div className="elementor-widget-wrap">
+                  <div
+                    className="elementor-element elementor-element-2d472405 elementor-widget elementor-widget-text-editor"
+                    data-id="2d472405"
+                    data-element_type="widget"
+                    id="roletotal"
+                    data-widget_type="text-editor.default"
+                  >
+                    <div className="elementor-widget-container">
+                      <div className="elementor-text-editor elementor-clearfix">
+                        <p
+                          className="auth-button"
+                          style={{ textAlign: "center" }}
+                        >
+                          <b>
+                            <i>Payment</i>
+                          </b>
+                        </p>{" "}
+                      </div>
+                    </div>
+                  </div>
+                  <div
+                    className="elementor-element elementor-element-24fc416 elementor-widget elementor-widget-html"
+                    data-id="24fc416"
+                    data-element_type="widget"
+                    data-widget_type="html.default"
+                  >
+                    <div className="elementor-widget-container">
+                      <iframe
+                        src="https://workflowai.online/shop/"
+                        width="290"
+                        height="400"
+                        allow="fullscreen"
+                      ></iframe>{" "}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>{" "}
+    </div>
+  );
+}
+export default Payments;
--- uxlivinglab/src/component/sidebar/sidebar.jsx
+++ uxlivinglab/src/component/sidebar/sidebar.jsx
@@ -1,18 +1,70 @@
 import "../sidebar/style.css";
 import { RxCross2 } from "react-icons/rx";
+import MyButton from "../popup/popup";
+import Payments from "../payments";
+import { useState } from "react";
 
 export default function Sidebar() {
+  const [changes, setChanges] = useState({
+    name: "component",
+    showHideLogin: false,
+    showHideLogout: false,
+    showHideProduct: false,
+    showHidePayments: false,
+    // showHideLogin: false,
+    // showHideLogin: false,
+    // showHideLogin: false,
+  });
+
+  const hideComponent = (name) => {
+    switch (name) {
+      case "showHideLogin":
+        setChanges({ showHideLogin: !changes.showHideLogin });
+        break;
+      case "showHideLogout":
+        setChanges({ showHideLogout: !changes.showHideLogout });
+        break;
+      case "showHidePayments":
+        setChanges({ showHidePayments: !changes.showHidePayments });
+        break;
+
+      default:
+        return null;
+    }
+  };
   return (
-    <>
-      <div id="grid-container">
+    <div id="grid-container">
+      <div id="second-container">
+        {/* <div className="close-second"> */}
+        {changes.showHideLogin && <MyButton text="Login" />}
+        {changes.showHideLogout && <MyButton text="Logout" />}
+        {changes.showHidePayments && <Payments text="Payments" />}
+        {/* <RxCross2 size={24} color="white" className="close" /> */}
+      </div>
+
+      {/* </div> */}
+
+      <div id="first-container">
         <div className="power">
-          <i aria-hidden="true" className="fas fa-power-off"></i>
+          <i
+            aria-hidden="true"
+            className="fas fa-power-off"
+            onClick={() => hideComponent("showHideLogout")}
+          ></i>
         </div>
         <div className="power">
-          <i aria-hidden="true" className="fas fa-sign-in-alt"></i>
+          <i
+            aria-hidden="true"
+            className="fas fa-sign-in-alt"
+            onClick={() => hideComponent("showHideLogin")}
+          ></i>
         </div>
         <div className="power">
-          <i aria-hidden="true" className="fas fa-credit-card"></i>
+          <i
+            aria-hidden="true"
+            className="fas fa-credit-card"
+            onClick={() => hideComponent("showHidePayments")}
+          ></i>
         </div>
         <div className="power">
           <i aria-hidden="true" className="fas fa-user"></i>
@@ -29,11 +81,15 @@ export default function Sidebar() {
         <div className="power">
           <i aria-hidden="true" className="fas fa-hand-holding-heart"></i>
         </div>
+        <div
+          onClick={
+            () => window.close()
+            // (document.querySelector("second-container").style.display = "none")
+          }
+        >
+          <RxCross2 size={24} color="red" className="close" />
+        </div>
       </div>
-      <div className="closeContainer">
-        {/* <h6 className="x">X</h6> */}
-        <RxCross2 size={24} color="white" />
-      </div>
-    </>
+    </div>
   );
 }
