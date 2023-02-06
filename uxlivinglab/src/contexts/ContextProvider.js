db078c6d5b9b6a5e3a3a15d496b2fe33386ca330
unknown <eske17.ea@gmail.com>
2023-02-06 11:51:42 +0300

    add product

--- uxlivinglab/src/contexts/ContextProvider.js
+++ uxlivinglab/src/contexts/ContextProvider.js
@@ -1,15 +1,48 @@
-import React, { createContext, useContext, useState } from "react";
+/* global chrome */
+import React, { createContext, useContext, useState, useEffect } from "react";
+import axios from "axios";
+
 const StateContext = createContext();
 
 export const ContextProvider = ({ children }) => {
   const [show, setShow] = useState(true);
+  const [sessionId, setSessionId] = useState(
+    "j57xy7zsvx9mi5sqnckdfgwqttnj698a"
+  );
+  const [data, setData] = useState();
 
   const handleShow = (show) => {
     setShow(show);
   };
 
+  useEffect(() => {
+    function logCookies(cookies) {
+      for (const cookie of cookies) {
+        if (cookie.domain === "100014.pythonanywhere.com") {
+          console.log(cookie.value);
+          // setSessionId(cookie.value);
+        }
+      }
+    }
+    // chrome.cookies
+    //   .getAll({
+    //     name: "sessionid",
+    //   })
+    //   .then((cookies) => logCookies(cookies));
+    async function getUserInfo() {
+      const response = await axios.post(
+        "https://100014.pythonanywhere.com/api/userinfo/",
+        {
+          session_id: sessionId,
+        }
+      );
+      setData(response.data.other_org);
+    }
+    getUserInfo();
+  }, []);
+
   return (
-    <StateContext.Provider value={{ show, handleShow }}>
+    <StateContext.Provider value={{ show, handleShow, sessionId, data }}>
       {children}
     </StateContext.Provider>
   );
