e7e9db4efdba65c594d513961ba88d29fc050136
unknown <eske17.ea@gmail.com>
2023-02-07 14:54:22 +0300

    add connect button

--- uxlivinglab/src/component/products/featureBox/FeatureBox.jsx
+++ uxlivinglab/src/component/products/featureBox/FeatureBox.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./styles.module.css";
 import { useStateContext } from "../../../contexts/ContextProvider";
 const FeatureBox = ({ product, org_name }) => {
-  const { data } = useStateContext();
+  const { data, sessionId } = useStateContext();
   return (
     <div className={styles.container}>
       <div className={styles.box}>
@@ -23,9 +23,24 @@ const FeatureBox = ({ product, org_name }) => {
             </option>
           ))} */}
         </select>
-        <button className={styles.button} style={{ fontSize: 20, width: 150 }}>
-          Connect
-        </button>
+        <a
+          href={
+            sessionId && product.title === "Workflow AI"
+              ? `https://ll04-finance-dowell.github.io/100018-dowellWorkflowAi-testing/#/?session_id=${sessionId}`
+              : ""
+          }
+          target={
+            sessionId && product.title === "Workflow AI" ? "_blank" : null
+          }
+          rel="noreferrer"
+        >
+          <button
+            className={styles.button}
+            style={{ fontSize: 20, width: 150 }}
+          >
+            Connect
+          </button>
+        </a>
       </div>
     </div>
   );
