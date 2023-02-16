ac8f7a4cb05cff37f173b9f6bdcb3dc8e6afcfa3
unknown <eske17.ea@gmail.com>
2023-02-16 16:30:39 +0300

    change favourites

--- uxlivinglab/src/component/favourites/favourites.js
+++ uxlivinglab/src/component/favourites/favourites.js
@@ -28,81 +28,83 @@ function Favourites() {
         </span>
       </div>
 
-      <div className="all">
-        <div class="elementor-form-fields-wrapper elementor-labels-above">
-          <div class="elementor-field-type-select elementor-field-group elementor-column elementor-field-group-level3name elementor-col-100">
-            <label for="form-field-level3name" class="elementor-field-label">
-              Select Product
-            </label>
-            <div class="elementor-field elementor-select-wrapper ">
-              <select
-                style={{ width: 350, marginTop: 2 }}
-                name="form_fields[level3name]"
-                id="form-field-level3name"
-                class="elementor-field-textual elementor-size-sm"
-              >
-                <option value="Product 01">Product 01</option>
-                <option value="Product 02">Product 02</option>
-                <option value="Product 03">Product 03</option>
-              </select>
-            </div>
-          </div>
-          <div class="elementor-field-type-select elementor-field-group elementor-column elementor-field-group-level3name elementor-col-100">
-            <label for="form-field-level3name" class="elementor-field-label">
-              Select Product
-            </label>
-            <div class="elementor-field elementor-select-wrapper ">
-              <select
-                style={{ width: 270, marginTop: 2 }}
-                name="form_fields[level3name]"
-                id="form-field-level3name"
-                class="elementor-field-textual elementor-size-sm"
-              >
-                <option value="Product 01">Product 01</option>
-                <option value="Product 02">Product 02</option>
-                <option value="Product 03">Product 03</option>
-              </select>
-            </div>
-          </div>
-          <div class="image">
-            <label for="form-field-field_a91fc81" class="elementor-field-label">
-              Upload Image{" "}
-            </label>
-            <input
-              type="file"
-              style={{ marginTop: 7 }}
-              name="form_fields[field_a91fc81]"
-              id="form-field-field_a91fc81"
-              class="elementor-field elementor-size-sm  elementor-upload-field"
-            />
-          </div>
-
-          <div class="button-div">
-            <button
-              type="submit"
-              style={{
-                width: 270,
-                cursor: "pointer",
-                fontSize: 15,
-                height: "13px",
-                borderWidth: "1px",
-                backgroundColor: "#7a7a7a",
-                marginBottom: 80,
-                color: "#ffffff",
-              }}
-              class="elementor-field-textual bt"
-              
-            >
-              Save / Remove Favourites
-            </button>
-          </div>
-        </div>
-      </div>
       {showProducts && (
-        <div>
-          <Products />
+        <div className="all">
+          <div class="elementor-form-fields-wrapper elementor-labels-above">
+            <div class="elementor-field-type-select elementor-field-group elementor-column elementor-field-group-level3name elementor-col-100">
+              <label for="form-field-level3name" class="elementor-field-label">
+                Select Product
+              </label>
+              <div class="elementor-field elementor-select-wrapper ">
+                <select
+                  style={{ width: 350, marginTop: 2 }}
+                  name="form_fields[level3name]"
+                  id="form-field-level3name"
+                  class="elementor-field-textual elementor-size-sm"
+                >
+                  <option value="Product 01">Product 01</option>
+                  <option value="Product 02">Product 02</option>
+                  <option value="Product 03">Product 03</option>
+                </select>
+              </div>
+            </div>
+            <div class="elementor-field-type-select elementor-field-group elementor-column elementor-field-group-level3name elementor-col-100">
+              <label for="form-field-level3name" class="elementor-field-label">
+                Select Product
+              </label>
+              <div class="elementor-field elementor-select-wrapper ">
+                <select
+                  style={{ width: 270, marginTop: 2 }}
+                  name="form_fields[level3name]"
+                  id="form-field-level3name"
+                  class="elementor-field-textual elementor-size-sm"
+                >
+                  <option value="Product 01">Product 01</option>
+                  <option value="Product 02">Product 02</option>
+                  <option value="Product 03">Product 03</option>
+                </select>
+              </div>
+            </div>
+            <div class="image">
+              <label
+                for="form-field-field_a91fc81"
+                class="elementor-field-label"
+              >
+                Upload Image{" "}
+              </label>
+              <input
+                type="file"
+                style={{ marginTop: 7 }}
+                name="form_fields[field_a91fc81]"
+                id="form-field-field_a91fc81"
+                class="elementor-field elementor-size-sm  elementor-upload-field"
+              />
+            </div>
+
+            <div class="button-div">
+              <button
+                type="submit"
+                style={{
+                  width: 270,
+                  cursor: "pointer",
+                  fontSize: 15,
+                  height: "13px",
+                  borderWidth: "1px",
+                  backgroundColor: "#7a7a7a",
+                  marginBottom: 80,
+                  color: "#ffffff",
+                }}
+                class="elementor-field-textual bt"
+              >
+                Save / Remove Favourites
+              </button>
+            </div>
+          </div>
         </div>
       )}
+      <div>
+        <Products />
+      </div>
 
       <div style={{ display: "flex" }}>
         <RxCross2
