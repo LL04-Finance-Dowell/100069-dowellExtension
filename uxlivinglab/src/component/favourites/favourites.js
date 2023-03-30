7ba3a92a1b4679dd48e1a2b4e56b2508f122d4df
Eskinder <eske17.ea@gmail.com>
2023-03-30 10:02:58 +0300

    new changes

--- uxlivinglab/src/component/favourites/favourites.js
+++ uxlivinglab/src/component/favourites/favourites.js
@@ -7,6 +7,10 @@ import Favorites from "./Favorites";
 import SendFavourites from "../../API/SendFavourites";
 import RemoveFavorites from "../../API/RemoveFavorites";
 import FetchFavourites from "../../API/FetchFavourites";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
 
 function Favourites() {
   const {
@@ -144,8 +148,35 @@ function Favourites() {
     }
   };
 
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+
   return (
     <div>
+      <Modal
+        open={true}
+        // onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
       <div className="item">
         <div className="{ props.text }">
           <button className="logout" style={{ width: 270, marginLeft: 14 }}>
