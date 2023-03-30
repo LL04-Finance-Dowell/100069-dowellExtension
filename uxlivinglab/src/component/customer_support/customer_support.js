b1b2bfaeaf82a2916b2c9c61666f2ef1ffa244cb
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-03-30 12:34:42 +0100

    updated chat and notifications

--- uxlivinglab/src/component/customer_support/customer_support.js
+++ uxlivinglab/src/component/customer_support/customer_support.js
@@ -1,10 +1,15 @@
 import "./customer_support.css";
 import { RxCross2 } from "react-icons/rx";
 import { useStateContext } from "../../contexts/ContextProvider";
+import { useState } from "react";
 
 function CustomerSupport() {
-  const { show, handleShow } = useStateContext();
+  const { show, handleShow, sessionId } = useStateContext();
+  const [buttonClicked, clickButton] = useState(false);
 
+  function clicker () {
+    clickButton(true);
+  }
   return (
     <div className="customer">
       <div className="item">
@@ -37,124 +42,38 @@ function CustomerSupport() {
           style={{
             width: "270px",
             borderTop: "1.5px solid red",
-            marginTop: "5px",
+            marginTop: "15px",
             marginLeft: 30,
           }}
         >
           {" "}
         </div>
-        <div
-          class="elementor-element elementor-element-82d1611 elementor-button-align-stretch elementor-widget elementor-widget-form"
-          data-id="82d1611"
-          data-element_type="widget"
-          data-settings='{"step_next_label":"Next","step_previous_label":"Previous","button_width":"100","step_type":"number_text","step_icon_shape":"circle"}'
-          data-widget_type="form.default"
-        >
-          <div class="elementor-widget-container">
-            <form class="elementor-form" method="post" name="Form1">
-              <input type="hidden" name="post_id" value="8155" />
-              <input type="hidden" name="form_id" value="82d1611" />
-              <input
-                type="hidden"
-                name="referer_title"
-                value="uxlivinglab - innovating from people's mind"
-              />
-              <input type="hidden" name="queried_id" value="8155" />
-
-              <div
-                class="elementor-form-fields-wrapper elementor-labels-above"
-                style={{ marginLeft: 30 }}
-              >
-                <div class="elementor-field-type-select elementor-field-group elementor-column elementor-field-group-level3name elementor-col-100">
-                  <label
-                    for="form-field-level3name"
-                    class="elementor-field-label"
-                  >
-                    Select Product
-                  </label>
-                  <div class="elementor-field elementor-select-wrapper ">
-                    <select
-                      name="form_fields[level3name]"
-                      id="form-field-level3name"
-                      class="elementor-field-textual elementor-size-sm"
-                      style={{ width: 284, height: 15 }}
-                    >
-                      <option value="Product 01">Product 01</option>
-                      <option value="Product 02">Product 02</option>
-                      <option value="Product 03">Product 03</option>
-                    </select>
-                  </div>
-                </div>
-
-                <button
-                  type="submit"
-                  class="connectWebsite"
-                  id="connectwebsite"
-                >
-                  Connect to Website
-                </button>
-                {/* </div> */}
-              </div>
-            </form>
-          </div>
+        {buttonClicked ?
+          <div style={{ marginRight: 10,marginTop:90, marginBottom:0 }}>
+          <iframe
+          id="frame"
+          title="Customer Support iframe"
+          style={{ alignItems: "center", border: 0 }}
+          src={`https://100096.pythonanywhere.com/chat/Extension/?session_id=${sessionId}`}
+          width="283"
+          height="200"
+          allow="fullscreen"
+        ></iframe>
         </div>
-        <div
-          class="elementor-divider"
-          style={{
-            width: "270px",
-            borderTop: "1.5px solid red",
-            marginTop: "5px",
-            marginLeft: 30,
-          }}
-        ></div>
-        <div class="elementor-widget-container">
-          <form class="elementor-form" method="post" name="Form1">
-            <input type="hidden" name="post_id" value="8155" />
-            <input type="hidden" name="form_id" value="2d0c3a0" />
-            <input
-              type="hidden"
-              name="referer_title"
-              value="uxlivinglab - innovating from people's mind"
-            />
-            <input type="hidden" name="queried_id" value="8155" />
 
-            <div
-              class="elementor-form-fields-wrapper elementor-labels-above"
-              style={{ marginLeft: 30 }}
-            >
-              <div class="elementor-field-type-select elementor-field-group elementor-column elementor-field-group-level3name elementor-col-100">
-                <div></div>
-                <label
-                  for="form-field-level3name"
-                  class="elementor-field-label"
-                >
-                  Select Product room{" "}
-                </label>
-                <div class="elementor-field elementor-select-wrapper ">
-                  <select
-                    name="form_fields[level3name]"
-                    id="form-field-level3name"
-                    class="elementor-field-textual elementor-size-sm"
-                    style={{ width: 284 }}
-                  >
-                    <option value="Product 01">Product 01</option>
-                    <option value="Product 02">Product 02</option>
-                    <option value="Product 03">Product 03</option>
-                  </select>
-                </div>
-              </div>
-              <div class="button-div">
-                <button
-                  type="submit"
-                  class="connectWebsite"
-                  id="chatcustomersupport"
-                >
-                  Chat with Customer Support
-                </button>
-              </div>
-            </div>
-          </form>
+          :
+          <div class="button-div" style={{marginTop:25,marginLeft:47}}>
+          <button
+            type="submit"
+            class="connectWebsite"
+            id="chatcustomersupport"
+            onClick={() => clicker()}
+          >
+            Chat with Customer Support
+          </button>
         </div>
+
+}
       </div>
       <div style={{ height: "40px" }}></div>
       <div style={{ display: "flex", marginLeft: 5 }}>
@@ -165,7 +84,7 @@ function CustomerSupport() {
           style={{
             backgroundColor: "red",
             borderRadius: 20,
-            marginTop: 4,
+            marginTop: 0,
             marginRight: 30,
             left: 0,
             marginBottom: 10,
