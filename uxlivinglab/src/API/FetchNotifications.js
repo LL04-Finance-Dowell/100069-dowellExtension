3a4b541343677d57a5c2a7d0d5febb58d12f6eb6
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-06-14 10:06:57 +0100

    changed endpoint for notifications

--- uxlivinglab/src/API/FetchNotifications.js
+++ uxlivinglab/src/API/FetchNotifications.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default async function FetchNotifications() {
-  return await axios.get("https://100092.pythonanywhere.com/notification/notification/")
+  return await axios.get("https://100092.pythonanywhere.com/api/v1/notifications/products/")
 }
