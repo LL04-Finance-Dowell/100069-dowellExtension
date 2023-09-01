9ad276264c1a5351505f06a32be819c34b9ff236
Eskinder <eske17.ea@gmail.com>
2023-09-01 08:57:50 +0300

    create project using vite

---
+++ README.md
@@ -0,0 +1,2 @@
+# 100069-dowellExtension
+The version 2.0 of extension . Designed and developed with react
---
+++ uxlivinglab/UxLivingLab/src/App.jsx
@@ -0,0 +1,12 @@
+// import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>Hello WOrld</div>
+    </>
+  );
+}
+
+export default App;
