a5013b7b04a5c23709f7428241060fbab8e2bf7e
Eskinder <eske17.ea@gmail.com>
2023-09-05 11:01:41 +0300

    add products UI design

--- uxlivinglab/UxLivingLab/src/page/Products/Product.jsx
+++ uxlivinglab/UxLivingLab/src/page/Products/Product.jsx
@@ -1,6 +1,7 @@
 import Dropdown from "react-dropdown";
 import HeaderComponent from "../../components/HeaderComponent";
 import styles from "./style.module.css";
+import { LiaAngleRightSolid, LiaAngleDownSolid } from "react-icons/lia";
 
 const options = ["one", "two", "three"];
 
@@ -14,8 +15,52 @@ export default function Product() {
         options={options}
         controlClassName={styles.controlClassName}
         menuClassName={styles.menuClassName}
-        
+        arrowClassName={styles.arrowClassName}
+        placeholderClassName={styles.placeholderClassName}
+        arrowOpen={
+          <LiaAngleDownSolid
+            size={12}
+            style={{ marginRight: 10, marginTop: 5 }}
+          />
+        }
+        arrowClosed={
+          <LiaAngleRightSolid
+            size={12}
+            style={{ marginRight: 10, marginTop: 5 }}
+          />
+        }
       />
+      <div
+        style={{
+          display: "flex",
+          columnGap: 50,
+          rowGap: 10,
+          marginLeft: 20,
+          flexWrap: "wrap",
+        }}
+      >
+        <div className={styles.products}>
+          <img
+            className={styles.product_image}
+            alt="product"
+            src="https://uxlivinglab.com/wp-content/uploads/2022/12/Digital-Q-1.png"
+          />
+        </div>
+        <div className={styles.products}>
+          <img
+            className={styles.product_image}
+            alt="product"
+            src="https://uxlivinglab.com/wp-content/uploads/2022/12/Digital-Q-1.png"
+          />
+        </div>
+        <div className={styles.products}>
+          <img
+            className={styles.product_image}
+            alt="product"
+            src="https://uxlivinglab.com/wp-content/uploads/2022/12/Digital-Q-1.png"
+          />
+        </div>
+      </div>
     </div>
   );
 }
