eb49bd6b2c7beb35e1ddb591c87cdb29c8812bc1
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-09-15 07:54:07 +0100

    Hide/See and Copy features added

--- uxlivinglab/src/component/payments/CreditListItem.js
+++ uxlivinglab/src/component/payments/CreditListItem.js
@@ -1,4 +1,17 @@
+import { AiOutlineEye, AiOutlineEyeInvisible, AiOutlineCopy } from "react-icons/ai";
+import { useState } from "react";
+
 export default function CreditListItem({ name, value }) {
+  const [showKey, setShowKey] = useState(false);
+  const [copied, setCopied] = useState(false);
+  function changeSeen() {
+    setShowKey(!showKey);
+  }
+  function copyItem() {
+    navigator.clipboard.writeText(value)
+    console.log("copied")
+    setCopied(true);
+  }
   return (
     <div
       style={{
@@ -21,7 +34,10 @@ export default function CreditListItem({ name, value }) {
         {name}:
       </span>
       {/* <span style={{marginLeft: 10}}></span> */}
-      <span style={{ marginLeft: 10 }}>{value}</span>
+      <span style={{ marginLeft: 10 }}>{name === "Service Key" ? (showKey ? <span style={{ fontSize: 11 }}>{value}</span> : <span style={{ fontSize: 16.5 }}> ********-****-****-****-*****</span>) : (value)}       <span style={{ paddingLeft: 13, cursor: "pointer" }} onClick={() => changeSeen()}>{name === "Service Key" && (showKey ? <AiOutlineEyeInvisible size={16} color="black" /> : <AiOutlineEye size={16} color="black" />)}</span>
+        {name === "Service Key" ? <span style={{ paddingLeft: 1, cursor: "pointer" }} onClick={() => copyItem()}><AiOutlineCopy size={16} color={copied ? "green" : "black"} /></span> : null}
+      </span>
+
     </div>
   );
 }
