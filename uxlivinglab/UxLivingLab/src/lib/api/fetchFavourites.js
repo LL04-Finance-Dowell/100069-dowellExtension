c189016d6b73f428c677480d7a77c5331d636394
Eskinder <eske17.ea@gmail.com>
2023-12-05 16:11:48 +0300

    add favorites

--- uxlivinglab/UxLivingLab/src/lib/api/fetchFavourites.js
+++ uxlivinglab/UxLivingLab/src/lib/api/fetchFavourites.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default async function FetchFavourites(userID) {
   return await axios.get(
-    `http://localhost:8000/api/v1/users/${userID}/favorites/`
+    `https://100092.pythonanywhere.com/api/v1/users/${userID}/favorites/`
     // `https://100092.pythonanywhere.com/favourite/favouriteImage/${username}`
   );
 }
